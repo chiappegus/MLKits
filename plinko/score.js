6425b6bf7253960b9268a4bd170506d2487fd157
gchiappe <gchiappe@magyp.gob.ar>
2018-10-26 16:15:54 -0300

    _sortBy()

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,15 @@
+const outputs = [];
+
+
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
-  console.log(dropPosition, bounciness);
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  //console.log(outputs);
+ console.log(_.sortBy(outputs, function(row){
+          return row[0] })
+ ); _.sortBy(outputs, function(row){
+          return row[0] });
 }
 
 function runAnalysis() {
