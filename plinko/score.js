2a387afa10c6706f1ee37024353f62545e592d7b
Gustavo <chiappegus@gmail.com>
2018-10-26 10:16:10 -0300

    COMIENZO :)

--- plinko/score.js
+++ plinko/score.js
@@ -1,5 +1,6 @@
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  console.log(dropPosition, bounciness);
 }
 
 function runAnalysis() {
